a0a6a9b30f1ce3eb5f6af4d5911c897b30b9fcf4
Leon Timmermans <fawaka@gmail.com>
2024-09-02 02:10:41 +0200

    Add ext argument to man1_pagename and man3_pagename

--- lib/ExtUtils/Helpers.pm
+++ lib/ExtUtils/Helpers.pm
@@ -19,8 +19,9 @@ BEGIN {
 }
 
 sub man1_pagename {
-	my $filename = shift;
-	return basename($filename).".$Config{man1ext}";
+	my ($filename, $ext) = @_;
+	$ext ||= $Config{man1ext};
+	return basename($filename).".$ext";
 }
 
 my %separator = (
@@ -32,12 +33,13 @@ my %separator = (
 my $separator = $separator{$^O} || '::';
 
 sub man3_pagename {
-	my ($filename, $base) = @_;
+	my ($filename, $base, $ext) = @_;
 	$base ||= 'lib';
+	$ext  ||= $Config{man3ext};
 	my ($vols, $dirs, $file) = splitpath(canonpath(abs2rel($filename, $base)));
 	$file = basename($file, qw/.pm .pod/);
 	my @dirs = grep { length } splitdir($dirs);
-	return join $separator, @dirs, "$file.$Config{man3ext}";
+	return join $separator, @dirs, "$file.$ext";
 }
 
 1;
@@ -70,11 +72,11 @@ This function splits a string the same way as the local platform does.
 
 This function substitutes a tilde at the start of a path with the users homedir in an appropriate manner.
 
-=func man1_pagename($filename)
+=func man1_pagename($filename, $ext = $Config{man1ext})
 
 Returns the man page filename for a script.
 
-=func man3_pagename($filename, $basedir)
+=func man3_pagename($filename, $basedir = 'lib', $ext = $Config{man3ext})
 
 Returns the man page filename for a Perl library.
 
